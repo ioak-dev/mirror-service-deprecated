10bba3cb291a3577a3864761ae1936e7d9b4cbec
Joshi <shrinivas87.joshi@gmail.com>
2019-10-26 01:08:47 +0530

    Alice-02: Updated the service and views

--- tenant/service.py
+++ tenant/service.py
@@ -2,6 +2,9 @@ import os, datetime, time
 from pymongo import MongoClient
 from library.db_connection_factory import get_collection
 
-def do_create(data):
-    tenant = get_collection('tenant').insert_one(data)
+DATABASE_URI = os.environ.get('DATABASE_URI')
+
+def do_create(tenant,data):
+    tenant = get_collection(tenant,'tenant').insert_one(data)
     return (200, {'_id': str(tenant.inserted_id)})
+
--- tenant/views.py
+++ tenant/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
 from django.core import serializers
+from tenant.service import do_create
 import json
 
 @api_view(['POST'])
-def create(request):
-    response = do_create(request.body)
+def create(request,tenant):
+    response = do_create(tenant, request.body)
     return JsonResponse(response[1], status=response[0])
