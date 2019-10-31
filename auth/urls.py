31556e96c6ecbadd2503f9f5b2c90cd3abcef727
Arun Kumar Selvaraj <dev@ioak.org>
2019-10-31 22:22:33 +0530

    JWT based user authentication implementation

--- auth/urls.py
+++ auth/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('keys/<str:email>/', views.getKeys),
     path('signup', views.signup),
     path('signin', views.signin),
-    path('jwttest', views.jwtTest)
+    path('jwttest', views.jwtTest),
+    path('signin/jwt', views.signin_jwt)
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
-from auth.service import generate_keys, get_keys, do_signup, do_signin, do_jwttest
+from auth.service import generate_keys, get_keys, do_signup, do_signin, do_jwttest, do_signin_via_jwt
 from django.core import serializers
 import json
 
@@ -24,8 +24,12 @@ def signin(request, tenant):
     response = do_signin(tenant, request.body)
     return JsonResponse(response[1], status=response[0])
 
+@api_view(['GET'])
+def jwtTest(request, tenant):
+    response = do_jwttest(tenant)
+    return HttpResponse(response[1], status=response[0])
 
 @api_view(['POST'])
-def jwtTest(request, tenant):
-    response = do_jwttest(tenant, request.body)
+def signin_jwt(request, tenant):
+    response = do_signin_via_jwt(tenant, request.body)
     return JsonResponse(response[1], status=response[0])
