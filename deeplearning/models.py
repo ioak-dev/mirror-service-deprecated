f7440b498f31e42a8bb33a3d4d6d1993805cc6e2
Arun Kumar Selvaraj <dev@ioak.org>
2019-11-28 17:06:42 +0530

    Implementation of RNN LSTM neural network for classification

--- deeplearning/models.py
+++ deeplearning/models.py
@@ -9,6 +9,15 @@ from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing import sequence
+
+
+max_words = 1000
+# time series i believe. set max_len to 1 for non-RNN
+max_len = 1
 
 class TransientModel:
     def __init__(self, tenant):
@@ -32,12 +41,14 @@ class TransientModel:
         })
         return features['features'], features['label']
 
-    def train(self, tenant):
-        train_dataset = tf.data.TFRecordDataset(filenames = ['data/tfrecords/' + tenant + '_' + 'train' + '.tfrecords']).map(self.tensor_to_tuple).shuffle(1000).batch(100)
-        val_dataset = tf.data.TFRecordDataset(filenames = ['data/tfrecords/' + tenant + '_' + 'val' + '.tfrecords']).map(self.tensor_to_tuple).shuffle(1000).batch(100)
-        test_dataset = tf.data.TFRecordDataset(filenames = ['data/tfrecords/' + tenant + '_' + 'test' + '.tfrecords']).map(self.tensor_to_tuple).shuffle(1000).batch(100)
-        self.neural_network(train_dataset, val_dataset, test_dataset)
-        self.model.save('data/model/' + tenant)
+    def train(self, tenant, label_map, train_df, test_df):
+        train_df.drop(['_id'],axis=1,inplace=True)
+        test_df.drop(['_id'],axis=1,inplace=True)
+        train_df['label'].replace(label_map, inplace=True)
+        test_df['label'].replace(label_map, inplace=True)
+        print(train_df)
+        self.neural_network(train_df, test_df)
+        # self.model.save('data/model/' + tenant)
 
     def predict(self, sentence):
         feature_vector = self.vectorize_sentence([sentence])
@@ -69,25 +80,71 @@ class TransientModel:
         score = classifier.score(X[2], y[2])
         print('Accuracy = ', score)
 
-    def neural_network(self, train_dataset, val_dataset, test_dataset):
-        inputs = keras.Input(shape=(100,), name='digits')
-        x = keras.layers.Dense(64, activation='relu', name='dense_1')(inputs)
-        x = keras.layers.Dense(64, activation='relu', name='dense_2')(x)
-        outputs = keras.layers.Dense(self.label_count, activation='softmax', name='predictions')(x)
-        self.model = keras.Model(inputs=inputs, outputs=outputs)
-        self.model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=1e-3),
-              loss='sparse_categorical_crossentropy',
-              metrics = ['accuracy'])
-        self.model.summary()
-        history = self.model.fit(train_dataset, epochs=30, validation_data=val_dataset)
+    def MLP(self):
+        inputs = keras.layers.Input(name='inputs',shape=[max_len])
+        layer = keras.layers.Embedding(max_words,50,input_length=max_len)(inputs)
+        layer = keras.layers.Dense(256,name='FC1')(layer)
+        layer = keras.layers.Activation('relu')(layer)
+        layer = keras.layers.Dropout(0.5)(layer)
+        layer = keras.layers.Dense(4,name='out_layer')(layer)
+        layer = keras.layers.Activation('sigmoid')(layer)
+        model = keras.Model(inputs=inputs,outputs=layer)
+        return model
+
+
+    def RNN(self):
+        inputs = keras.layers.Input(name='inputs',shape=[max_len])
+        layer = keras.layers.Embedding(max_words,50,input_length=max_len)(inputs)
+        layer = keras.layers.LSTM(64)(layer)
+        layer = keras.layers.Dense(256,name='FC1')(layer)
+        layer = keras.layers.Activation('relu')(layer)
+        layer = keras.layers.Dropout(0.5)(layer)
+        layer = keras.layers.Dense(4,name='out_layer')(layer)
+        layer = keras.layers.Activation('sigmoid')(layer)
+        model = keras.Model(inputs=inputs,outputs=layer)
+        return model
         
-        loss, accuracy = self.model.evaluate(train_dataset)
+    def neural_network(self, train_df, test_df):
+        le = LabelEncoder()
+        X_train = train_df['text']
+        # y_train = train_df['label'].to_numpy().reshape(-1,1)
+        y_train = le.fit_transform(train_df['label']).reshape(-1,1)
+        X_test = test_df['text']
+        # y_test = test_df['label'].to_numpy().reshape(-1,1)
+        y_test = le.fit_transform(test_df['label']).reshape(-1,1)
+
+        tok = Tokenizer(num_words=max_words)
+        tok.fit_on_texts(X_train)
+        sequences = tok.texts_to_sequences(X_train)
+        sequences_matrix = sequence.pad_sequences(sequences,maxlen=max_len)
+
+        self.model = self.RNN()
+        self.model.summary()
+        self.model.compile(loss='sparse_categorical_crossentropy',optimizer=RMSprop(),metrics=['accuracy'])
+        history = self.model.fit(sequences_matrix,y_train,batch_size=128,epochs=10, validation_split=0.2)
+
+        # inputs = keras.Input(shape=(100,), name='digits')
+        # x = keras.layers.Dense(64, activation='relu', name='dense_1')(inputs)
+        # x = keras.layers.Dense(64, activation='relu', name='dense_2')(x)
+        # outputs = keras.layers.Dense(4, activation='softmax', name='predictions')(x)
+        # self.model = keras.Model(inputs=inputs, outputs=outputs)
+        # self.model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=1e-3),
+        #       loss='sparse_categorical_crossentropy',
+        #       metrics = ['accuracy'])
+        # self.model.summary()
+        # history = self.model.fit(train_dataset, epochs=30, validation_data=val_dataset)
+        
+        test_sequences = tok.texts_to_sequences(X_test)
+        test_sequences_matrix = sequence.pad_sequences(test_sequences,maxlen=max_len)
+
+        train_sequences = tok.texts_to_sequences(X_train)
+        train_sequences_matrix = sequence.pad_sequences(train_sequences,maxlen=max_len)
+
+        loss, accuracy = self.model.evaluate(train_sequences_matrix, y_train)
         print("Training Accuracy:  {:.4f}".format(accuracy))
-        loss, accuracy = self.model.evaluate(val_dataset)
-        print("Validation Accuracy:  {:.4f}".format(accuracy))
-        loss, accuracy = self.model.evaluate(test_dataset)
+        loss, accuracy = self.model.evaluate(test_sequences_matrix, y_test)
         print("Testing Accuracy:  {:.4f}".format(accuracy))
-        # self.plot_history(history)
+        self.plot_history(history)
     
     def plot_history(self, history):
         acc = history.history['accuracy']
