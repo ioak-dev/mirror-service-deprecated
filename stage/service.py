978dfbf64e1066564bdd3b9deb0274a2fdd907b4
Joshi <shrinivas87.joshi@gmail.com>
2019-11-30 16:04:20 +0530

    Alice-07 : Updated the service as per review

--- stage/service.py
+++ stage/service.py
@@ -6,17 +6,20 @@ import base64
 from bson.binary import Binary
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
+import json
 
-def do_add_stage(tenant, stage):
-    all_stages = get_collection(tenant,'stage').find({}, {'_id': False})
-    id = 0
-    for j in all_stages :
-        for i in stage :
-            if j['name'] not in i :
-                id = get_collection(tenant, 'stage').remove({'name' : j["name"]})
-    for i in stage :
-        tempStage = get_collection(tenant,'stage').find_one({'name': i['name']})
-        if tempStage is None :
-            id = get_collection(tenant, 'stage').insert_one(i)
-            
-    return (200, {'_id': str(id)})
+#get existing stages to display
+def do_get_stages(tenant):
+    existing_stage_list = get_collection(tenant,'stage').find()
+    existing_stage = []
+    for existing_stages in existing_stage_list:
+        existing_stages['_id'] = str(existing_stages['_id'])
+        existing_stage.append(existing_stages)
+    return (200, (existing_stage))
+
+# update or remove or insert stages
+def do_update_stages(tenant, list_stage_latest):
+    get_collection(tenant, 'stage').remove()
+    local_stage = get_collection(tenant, 'stage').insert_many(list_stage_latest)
+    return (200, {'_id': str(local_stage.inserted_ids)})
+    
--- stage/urls.py
+++ stage/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns =[
-    path('', views.add_stage)
+    path('', views.get_update_stages)
 ]
