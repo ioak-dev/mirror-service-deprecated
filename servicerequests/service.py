f3c424ae2fba8c7db4eae14012c4721498f5d653
Joshi <shrinivas87.joshi@gmail.com>
2020-01-18 22:12:49 +0530

    Belle-01: Update request details added

--- servicerequests/service.py
+++ servicerequests/service.py
@@ -1,6 +1,7 @@
 import os, datetime, time, random
 from pymongo import mongo_client
 from library.db_connection_factory import get_collection
+from bson.objectid import ObjectId
 
 DATABASE_URI = os.environ.get('DATABASE_URI')
 
@@ -12,14 +13,44 @@ def do_get_sr(tenant):
         existing_srs['createDate'] = str(existing_srs['createDate'])
         existing_srs['updateDate'] = str(existing_srs['updateDate'])
         existing_sr.append(existing_srs)
+    print(existing_sr)
     return (200, {'data': existing_sr})
 
 def do_add_update_sr(tenant, data):
-    stage_list = list(get_collection(tenant, 'stage').find({}))
-    data['createDate'] = datetime.datetime.now().isoformat()
-    data['updateDate'] = datetime.datetime.now().isoformat()
-    data['status'] = 'assigned'
-    data['stage'] = stage_list[0]['name']
-    data['assignedTo'] = ''
-    request = get_collection(tenant, 'serviceRequests').insert_one(data)
-    return (200, {'_id': str(request.inserted_id)})
+    updated_id = 'id'
+    stage_id ='stageId'
+    if updated_id not in data and stage_id not in data :
+        stage_list = list(get_collection(tenant, 'stage').find({}))
+        #data['createDate'] = datetime.datetime.now().isoformat()
+        #data['updateDate'] = datetime.datetime.now().isoformat()
+        data['status'] = 'assigned'
+        #data['priority'] = 'Low'
+        data['stage'] = stage_list[0]['name']
+        data['assignedTo'] = ''
+        request = get_collection(tenant, 'serviceRequests').insert_one(data)
+        return (200, {'_id': str(request.inserted_id)})
+    elif updated_id not in data and stage_id in data :
+        id = get_collection(tenant, 'serviceRequests').update_one({
+            '_id' : ObjectId(data['stageId'])
+        },{
+            '$set':{
+                'stage' : data['stage'],
+                'previousStage' : data['previousStage'],
+                'assignedTo': ''
+            }
+        }, upsert=True)
+        return (200, {'_id': str(id)})
+    else:
+        id = get_collection(tenant, 'serviceRequests').update_one({
+            '_id' : ObjectId(data['id'])
+        },{
+            '$set':{
+                'title' : data['title'],
+                'description' : data['description'],
+                'priority' : data['priority'],
+                'updateDate': data['updateTime'],
+                'comments': data['comment'],
+                'assignedTo': data['assignedTo']
+            }
+        }, upsert=True)
+        return (200, {'_id': str(id)})
