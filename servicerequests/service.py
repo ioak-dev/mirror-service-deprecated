44d1b18f291445057aabbd69f5c5414fa0f6df52
Joshi <shrinivas87.joshi@gmail.com>
2020-01-20 23:36:48 +0530

    Belle:01 - Updated as per comments

--- servicerequests/service.py
+++ servicerequests/service.py
@@ -3,15 +3,13 @@ from pymongo import mongo_client
 from bson.objectid import ObjectId
 import library.db_utils as db_utils
 
-domain=["serviceRequests","stage"]
+domain="serviceRequests"
 
 def do_get_sr(request, tenant):
-    data = db_utils.find(tenant, domain[0], {})
+    data = db_utils.find(tenant, domain, {})
     return (200, {'data': data})
 
 
 def do_add_update_sr(request, tenant):
-    stage_list = db_utils.find(tenant, domain[1],{})
-    data['stage'] = stage_list[0]['name']
-    updated_record = db_utils.upsert(tenant, domain[0], request.body, request.user_id)
+    updated_record = db_utils.upsert(tenant, domain, request.body, request.user_id)
     return (200, {'data': updated_record})
