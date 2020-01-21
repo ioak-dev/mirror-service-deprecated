8c3761f25ced88c1de7c877e81f025a924f31464
Arun Kumar Selvaraj <dev@ioak.org>
2020-01-21 12:51:30 +0530

    service request domain broken down into two collections

--- servicerequests/service.py
+++ servicerequests/service.py
@@ -3,13 +3,21 @@ from pymongo import mongo_client
 from bson.objectid import ObjectId
 import library.db_utils as db_utils
 
-domain="serviceRequests"
+domain_sr_main="servicerequests.main"
+domain_sr_log="servicerequests.log"
 
-def do_get_sr(request, tenant):
-    data = db_utils.find(tenant, domain, {})
+def get_sr_main(request, tenant):
+    data = db_utils.find(tenant, domain_sr_main, {})
     return (200, {'data': data})
 
+def update_sr_main(request, tenant):
+    updated_record = db_utils.upsert(tenant, domain_sr_main, request.body, request.user_id)
+    return (200, {'data': updated_record})
 
-def do_add_update_sr(request, tenant):
-    updated_record = db_utils.upsert(tenant, domain, request.body, request.user_id)
+def get_sr_log(request, tenant):
+    data = db_utils.find(tenant, domain_sr_log, {})
+    return (200, {'data': data})
+
+def update_sr_log(request, tenant):
+    updated_record = db_utils.upsert(tenant, domain_sr_log, request.body, request.user_id)
     return (200, {'data': updated_record})
